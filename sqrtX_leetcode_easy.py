5882bbe44c6c4bdf4080634ed0d3ad5d7276d6fe
Rejoan Siddiky <siddikyrejoan@gmail.com>
2022-04-03 14:09:35 +0600

    sqrt(x) leetcode solution

---
+++ sqrtX_leetcode_easy.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def mySqrt(self, x):
+        l = 0
+        r = x
+        res = 0
+        
+        while(l <= r):
+            m = (l + r) // 2
+            
+            if m * m <= x:
+                res = m
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return res
+        
+
+ # problem link: https://leetcode.com/problems/sqrtx/       
